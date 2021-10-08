12aae449df3af42a1f23cb92ae13626f572ac9cf
Daniel <jones.89.dan@gmail.com>
2021-10-07 20:34:52 -0700

    formated output, added comments, removed unused variables

--- Tester.java
+++ Tester.java
@@ -1,6 +1,6 @@
 /**
  * Adds large numbers using Stacks of various lengths
- * 
+ *
  * @author Daniel Jones
  * @version 10/6/2021
  */
@@ -19,61 +19,75 @@ public class Tester
         //same length
         String first = "2147483647";
         String second = "2147483647";
-        
-        //you must write all of your code here
-        
+
+        //stacks to be used in the program.
         StackInterface<Integer> firstNum = new LinkedStack<Integer>();
         StackInterface<Integer> secondNum = new LinkedStack<Integer>();
         StackInterface<Integer> resultNum = new LinkedStack<Integer>();
-        
+
+        //instance variables.
         int len1 = first.length();
         int len2 = second.length();
-        int extraLen= 0;
-        int lenAdd = 0;
         int result = 0;
         int carryOver = 0;
-       
-        
-        
+
+        //loop through each char in the number string, convert to int and add to the first stack.
         for (int i = 0; i < len1; ++i) {
             int x = (Integer) (first.charAt(i) - '0');
             firstNum.push(x);
         }
-    
+
+        //loop through each char in the number string, convert to int and add to the second stack.
         for (int i = 0; i < len2; ++i) {
             int x = (Integer) (second.charAt(i) - '0');
             secondNum.push(x);
         }
-        
+
+        //loop through each stack if not empty.
         while (!firstNum.isEmpty() || !secondNum.isEmpty()) {
-            
+
             for (int i = 0; i < len1; ++i) {
-                
+
+                //calcuate the result by adding number from frist and second stack.
                 result = getNum(firstNum) + getNum(secondNum);
-                
+
+                //add carryover if value exists and clears value.
                 if(carryOver != 0) {
                      result += carryOver;
                      carryOver = 0;
                 }
-                
+
+                //calculates the carry over and result.
                 if (result >= 10) {
                     carryOver = result / 10;
                     result = result % 10;
                 }
-                
+
+                //push result to stack.
                 resultNum.push(result);
             }
         }
-                
-        while (!resultNum.isEmpty())    
-           System.out.print(resultNum.pop()); 
+
+        //formated outup.
+        System.out.print(first + " + " + second + " = ");
+
+        while (!resultNum.isEmpty())
+           System.out.print(resultNum.pop());
+
+        System.out.print("\n");
     }
-    
-    public static int getNum(StackInterface<Integer> s) 
+
+    /**
+    * Retreives int from the stack or returns zero if stack is empty.
+    * @param s - stack to pop.
+    * @return s.pop - popped item from the stack or zero if stack is empty.
+    */
+    public static int getNum(StackInterface<Integer> s)
     {
+        //if stack is empty return 0, else pop the next int.
         if (s.isEmpty()) {
             return 0;
-	} 
+	}
     	else {
             return s.pop();
 	}
